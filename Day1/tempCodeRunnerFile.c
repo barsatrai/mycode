66ea8a93d88f28dd607927b9df24a73b93794e99
Barsat Rai <barsatrai777@gmail.com>
2022-12-22 11:52:57 +0545

    Day1 complete

---
+++ Day1/tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void main()
+{    
+    int x,y;
+    printf("Enter the value of x and y:");
+    scanf("%d%d", &x, &y);
+    printf("the value you entered is:",x,y);
+}
